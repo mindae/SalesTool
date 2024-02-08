274ffeac90231dda0e94904275553c967e5390a0
mindae <MindaeLaddoo01>
2024-02-08 10:53:47 +0530

    initial commit

---
+++ SalesTool/src/mindae/demo/salestool/Main.java
@@ -0,0 +1,18 @@
+package mindae.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("HELLO happy sales people");
+		System.out.println("THIS app shows sales data");
+	}
+
+}
---
+++ SalesTool/src/mindae/demo/salestool/SalesData.java
@@ -0,0 +1,10 @@
+package mindae.demo.salestool;
+
+public class SalesData {
+	private int[] data = {0, 4, 42};
+	
+	public void display() {
+		System.out.println("TODO: implement sales data.display()");
+	}
+
+}
