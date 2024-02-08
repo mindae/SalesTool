102456b433275a6c88e2d00a5720651c0057c85c
mindae <mahendrachaurasia@gmail.com>
2024-02-08 12:18:31 +0530

    Summed up the sales data

--- SalesTool/src/mindae/demo/salestool/SalesData.java
+++ SalesTool/src/mindae/demo/salestool/SalesData.java
@@ -1,15 +1,18 @@
 package mindae.demo.salestool;
 
 import java.util.Iterator;
-
+/*represents the sales data*/
 public class SalesData {
 	private int[] data = {0, 4, 42};
 	
 	public void display() {
 		System.out.println("Data: ");
+		int sum=0;
 		for(int i=0;i<data.length;i++) {
 			System.out.println("Next Value: "+data[i]);
+			sum+=i;
 		}
+		System.out.println("Sum is: "+sum);
 	}
 
 }
