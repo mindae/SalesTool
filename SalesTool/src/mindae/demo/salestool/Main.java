9fc4af19f3a6548599ca5a5eb55f775b173a9c20
mindae <MindaeLaddoo01>
2024-02-08 11:07:39 +0530

    added new output msg.

--- SalesTool/src/mindae/demo/salestool/Main.java
+++ SalesTool/src/mindae/demo/salestool/Main.java
@@ -13,6 +13,7 @@ public class Main {
 	private static void displayGreeting() {
 		System.out.println("HELLO happy sales people");
 		System.out.println("THIS app shows sales data");
+		System.out.println("test 1");
 	}
 
 }
